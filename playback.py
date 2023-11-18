b00f2abb328809ed36492bfd79a81f469cce40a4
Bryan Castellucci <90343916+Pianorules20@users.noreply.github.com>
2023-11-18 01:30:11 -0500

    Add files via upload
    updated playback.py

--- playback.py
+++ playback.py
@@ -1,5 +1,6 @@
 #play.py 
 import mySong, generate_notes_interface, playbackMeter, settings, pygame, gatekeeper, timer as t
+import plot_notes_interface as pni
 from pygame.locals import *
 
 #timer = pygame.time.get_ticks()
@@ -11,33 +12,10 @@ class Player():
 
     def play():
 
-        t.Timer.current = t.Timer.fast
-        print('playing!')
+        t.Timer.current = t.Timer.slow
+        
+        for eachPlot in Player.recording:
 
-        #Player.clock.tick(round(settings.Preferences.metronome/settings.Preferences.metronomeModifier))
-                
-        '''for eachNote in eachSection:
-
-            xPos = create_note.Note.returnXPos(eachNote)
-            if xPos == playbackMeter.Meter.meter:  # this is the most important line hereS
-                name = create_note.Note.returnLetterName(eachNote)
-                octave = create_note.Note.returnOctave(eachNote)
-                yPos = create_note.Note.returnYPos(eachNote) 
-                sound = create_note.Note.returnSound(eachNote)           
-                try:
-                    create_note.Note.play(sound)
-                    print(f'playing note data xPos {xPos} {name}{octave} yPos{yPos}')
-                    section -= 1
-                    print(f'keep going! {section}')
-                except Exception as e:
-                    print('Exception during play.Player.play')
-                    print(e)
-                            
-                        else:
-                            pass
-                    playbackMeter.Meter.advance()
-            else:
-                playbackMeter.Meter.reset()'''
-        #mySongs.Recordings.play = False
+                pni.P.play(eachPlot)
         
         #TROUBLESHOOT NOTES I AM ONLY GETTING ONE NOTE AT A TIME WHY IS THAT?
